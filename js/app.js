4f79859a6a59cc3e0c96f2db36646309fec81cb1
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2022-05-17 20:23:52 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 let searchBtn = document.querySelector(".bx-search");
-let arrow = document.querySelector('arrow-round');
+let arrow = document.querySelector('.arrow-round');
 
 btn.onclick = function() {
     sidebar.classList.toggle("active");
     btn.classList.toggle('rotate')
+}
+
+var mq = window.matchMedia("(max-width: 766px)");
+if (mq.matches) {
+    // window width is at less than 570px
+    sidebar.classList.remove('active')
+    arrow.style.display = "none";
+} else {
+    // window width is greater than 570px
 }
